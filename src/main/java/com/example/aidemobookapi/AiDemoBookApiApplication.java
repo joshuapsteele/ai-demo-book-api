71909fac9ce50b5bde31693a71629dd7b77a2d4a
Joshua Steele <jsteele@proofpoint.com>
2023-06-07 10:58:56 -0400

    Initial setup

---
+++ src/main/java/com/example/aidemobookapi/AiDemoBookApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.aidemobookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDemoBookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiDemoBookApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aidemobookapi/AiDemoBookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aidemobookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDemoBookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
